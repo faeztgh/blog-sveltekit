0175be4e9101c9b21a73d2e800eeabe236b98f22
faeztgh <d.jkh98@gmail.com>
2022-08-08 14:58:22 +0430

    Updates

--- _app/immutable/chunks/logo-ae170d24.js
+++ _app/immutable/chunks/logo-ae170d24.js
@@ -1,1 +1,1 @@
-const t="https://github.com/faeztgh/blog-sveltekit/_app/immutable/assets/logo-e5edf434.svg";export{t as l};
+const t="https://faeztgh.github.io/blog-sveltekit/_app/immutable/assets/logo-e5edf434.svg";export{t as l};
