8765c29a93f0bbdd455aee87aebc1d9038a5ec5a
faeztgh <d.jkh98@gmail.com>
2022-08-08 11:44:05 +0430

    init

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,33 @@
-import adapter from '@sveltejs/adapter-auto';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = 'production' === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess( preprocess({
-		postcss: true,
-	  }),),
+	preprocess: preprocess(
+		preprocess({
+			postcss: true
+		})
+	),
 
 	kit: {
-		adapter: adapter()
+		prerender: {
+			default: true
+		},
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		paths: {
+			// change below to your repo name
+			base: dev ? '' : '/blog-sveltekit'
+		}
 	}
 };
 
